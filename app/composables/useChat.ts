c3e64b083d13906bd0d11c1eaac442fceda296bf
BartoszStachowski <stachowskibartosz96@gmail.com>
2025-09-03 11:28:46 +0200

    composables

---
+++ app/composables/useChat.ts
@@ -0,0 +1,31 @@
+import type { Chat, ChatMessage } from '@/types';
+import { MOCK_CHAT } from '@/composables/mockData';
+
+export const useChat = () => {
+  const chat = ref<Chat>(MOCK_CHAT);
+  const messages = computed<ChatMessage[]>(() => chat.value.messages);
+
+  const createMessage = (message: string, role: ChatMessage['role']) => {
+    const id = messages.value.length.toString();
+
+    return {
+      id,
+      role,
+      content: message,
+    };
+  };
+
+  const sendMessage = (message: string) => {
+    messages.value.push(createMessage(message, 'user'));
+
+    setTimeout(() => {
+      messages.value.push(createMessage(`You said: ${message}`, 'assistant'));
+    }, 200);
+  };
+
+  return {
+    chat,
+    messages,
+    sendMessage,
+  };
+};
